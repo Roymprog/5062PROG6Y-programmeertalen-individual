9539a6f229cf2ccdd18006eb96efec6f259972f6
Roy van Santen <roy.van.santen@ing.nl>
2019-02-25 15:02:47 +0100

    Refactor opgave1 and opgave2

--- python-individual/individual.py
+++ python-individual/individual.py
@@ -1,7 +1,7 @@
-import collections
 import sys
 
-def opgave1(mylist):
+
+def get_all_values(mylist):
     if not isinstance(mylist, (list, range)):
         raise Exception('This function should only be called with a list as parameter')
 
@@ -10,18 +10,20 @@ def opgave1(mylist):
     if (n == 0):
         return False
 
-    all_values = (range(1, n + 1))
-    return all([ x in mylist for x in all_values ])
+    return range(1, n + 1)
+
+
+def opgave1(mylist):
+    all_values = get_all_values()
+
+    return all([x in mylist for x in all_values])
+
 
 def opgave2(mylist):
-    if not isinstance(mylist, (list, range)):
-        raise Exception('This function should only be called with a list as parameter')
-
-    n = len(mylist)
-
-    all_values = (range(1, n + 1))
+    all_values = get_all_values()
 
     return (n for n in list(filter(lambda x: x not in mylist, all_values)))
+
 
 def opgave3a(filename):
     grid = []
@@ -30,10 +32,11 @@ def opgave3a(filename):
             row = []
             string = line.strip()
             for char in string:
-                if (char != ' '):
+                if char != ' ':
                     row.append(int(char))
             grid.append(row)
     return grid
+
 
 def opgave3b(filename):
     mylist = opgave3a(filename)
@@ -42,14 +45,16 @@ def opgave3b(filename):
             sys.stdout.write(str(x) + " ")
         print("")
 
+
 def opgave3(filename):
     opgave3b( opgave3a( filename ) )
 
+
 def sum_nested_it(mylist):
-    sum = 0
+    som = 0
     for x in mylist:
-        if (isinstance(x, (list, range))):
+        if isinstance(x, (list, range)):
             mylist.extend(x)
         else:
-            sum = sum + x
-    return sum
+            som = som + x
+    return som
